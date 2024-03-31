3768ba09143cdebe4ecb20537ed2f600b9bffeec
zeraibiyoucef <zeraibiyoucef1@gmail.com>
2024-03-31 04:33:07 +0100

    set up hot toasts and use it when deleting the cabins

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter,Routes,Route, Navigate} from  'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
 
 import GlobalStyles from "./styles/GlobalStyles"
 import Dashboard from './pages/Dashboard'
@@ -42,6 +43,21 @@ const App = () => {
           <Route path='*' element={<PageNoteFound/>}/>
         </Routes>
       </BrowserRouter>
+      <Toaster position="top-center" gutter={12} containerStyle={{margin:"8px"}} toastOptions={{
+        success:{
+          duration:3500,
+        },
+        error:{
+          duration:5000,
+        },
+        style:{
+          fontSize:"16px",
+          maxWidth:"500px",
+          padding:"16px 24px",
+          backgroundColor:"var(--color-grey-8)",
+          color:"var(--color-grey-700)",
+        }
+      }}/>
     </QueryClientProvider>
   );
 }
